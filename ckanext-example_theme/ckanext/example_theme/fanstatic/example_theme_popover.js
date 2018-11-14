b9d3a6d131810c87c4c2ba3cbc37bf20f39200d1
Odd Andersen <odd.andersen@sintef.no>
2018-11-14 18:12:49 +0100

    Finished CKAN tutorial up to 6.5.8

--- ckanext-example_theme/ckanext/example_theme/fanstatic/example_theme_popover.js
+++ ckanext-example_theme/ckanext/example_theme/fanstatic/example_theme_popover.js
@@ -3,17 +3,58 @@
 ckan.module('example_theme_popover', function ($) {
     return {
 	initialize: function () {
-	    var num_resources = this.options.num_resources;
-	    var license = this.options.license;
-	    var content = 'NUM resources, LICENSE'
-		.replace('NUM', this.options.num_resources)
-	        .replace('LICENSE', this.options.license)
 
-	    this.el.popover({title: this.options.title,
-			     content: content,
-			     placement: 'left'});
-	    
-	    console.log("I've been initialized again for element: ", this.el);
+	    $.proxyAll(this, /_on/);
+
+	    // this.el.popover({title: this.options.title, html: true,
+	    // 		     content: this._('Loading...'), placement: 'left'});
+
+	    this.el.on('click', this._onClick);
+	    this.sandbox.subscribe('dataset_popover_clicked',
+					  this._onPopoverClicked);
+	},
+
+	teardown: function() {
+	    this.sandbox.unsubscribe('dataset_popover_clicked',
+				     this._onPopoverClicked());
+	},
+	
+	_snippedReceived: false,
+	
+	_onClick: function(event) {
+	    if (!this._snippedReceived) {
+		this.sandbox.client.getTemplate('example_theme_popover.html',
+						this.options,
+						this._onReceiveSnippet,
+						this._onReceiveSnippetError);
+		this._snippedReceived = true;
+	    }
+	    this.sandbox.publish('dataset_popover_clicked', this.el);
+	},
+
+	_onPopoverClicked: function(button) {
+	    if (button != this.el) {
+		this.el.popover('hide');
+	    }
+	},
+	
+	_onReceiveSnippet: function(html) {
+	    this.el.popover('destroy');
+
+	    this.el.popover({title: this.options.title, html: true,
+	    		     content: html, placement: 'left'});
+
+	    this.el.popover('show');
+	},
+
+	_onReceiveSnippetError: function(error) {
+	    this.el.popover('destroy');
+
+	    var content = error.status + ' ' + error.statusText + ' :(';
+	    this.el.popover({title: this.options.title, html: true,
+			     content: content, placement: 'left'});
+	    this.el.popover('show');
+	    this_.snippetReceived = true;
 	}
     };
 });
