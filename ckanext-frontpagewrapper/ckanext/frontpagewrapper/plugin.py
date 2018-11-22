27d3b7aeb8e1af9a1fb2155034bccbd1b2634215
Odd <odd.andersen@sintef.no>
2018-11-22 23:17:01 +0100

    Now the frontpagewrapper redirects everyone except logged-in users

--- ckanext-frontpagewrapper/ckanext/frontpagewrapper/plugin.py
+++ ckanext-frontpagewrapper/ckanext/frontpagewrapper/plugin.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-
+#from flask.globals import _request_ctx_stack, request, session, g
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import pdb
@@ -8,18 +8,54 @@ import pdb
 def custom_action():
     return "Hello World!"
 
+class TestMiddleware:
+    def __init__(self, app):
+        #pdb.set_trace()
+        self.app = app
+
+        try:
+            @app.before_request
+            def tull():
+                # only let logged-in users access site
+                if toolkit.request.remote_user is None:
+                    return toolkit.render("frontpagewrapper.html", {})
+                    #return "Hello World~!"
+
+        except Exception as e:
+            pass 
+        
+    def __call__(self, environ, start_response):
+        #pdb.set_trace()
+
+        #return self.app.finalize_request("Hello world")
+        #return "Hello world"
+        #krull = self.app(environ,start_response)
+        return self.app(environ,start_response)
+
 
 class FrontpagewrapperPlugin(plugins.SingletonPlugin):
 
-    plugins.implements(plugins.IBlueprint)
+    plugins.implements(plugins.IMiddleware)
+    plugins.implements(plugins.IConfigurer)
 
-    def get_blueprint(self):
-        blueprint = Blueprint('foo', self.__module__)
-        rules = [
-            ('/', 'custom_action', custom_action),
-        ]
-        for rule in rules:
-            blueprint.add_url_rule(*rule)
+    def update_config(self, config):
+        toolkit.add_template_directory(config, 'templates')
 
-        return blueprint
+    def make_middleware(self, app, config):
+        #pdb.set_trace()
+        app = TestMiddleware(app)
+        return app
+        
+    
+    # plugins.implements(plugins.IBlueprint)
+
+    # def get_blueprint(self):
+    #     blueprint = Blueprint('foo', self.__module__)
+    #     rules = [
+    #         ('/', 'custom_action', custom_action),
+    #     ]
+    #     for rule in rules:
+    #         blueprint.add_url_rule(*rule)
+
+    #     return blueprint
     
