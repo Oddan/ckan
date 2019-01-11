d0b39186dad435b5b798096ecf559aa9953081b7
FilippoRemonato <filippo.remonato@sintef.no>
2019-01-11 13:18:28 +0100

    PLUGIN: RESTRICT_USER_PERMISSION. The scope of this plugin is to restric what a user can do/click on in the prototype website. Implemented removal of unneeded button/links from the top navigation bars.

---
+++ ckanext-restrict_user_permissions/ckanext/restrict_user_permissions/plugin.py
@@ -0,0 +1,13 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+class Restrict_User_PermissionsPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'restrict_user_permissions')
