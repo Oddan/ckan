5a80975425f649db697741ef900f0d34512999a2
FilippoRemonato <filippo.remonato@sintef.no>
2019-01-11 14:33:46 +0100

    PLUGIN: RESTRICT_USER_PERMISSIONS. Implemented remove user access to modifying its own settings

--- ckanext-restrict_user_permissions/ckanext/restrict_user_permissions/plugin.py
+++ ckanext-restrict_user_permissions/ckanext/restrict_user_permissions/plugin.py
@@ -1,9 +1,16 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+# This function prohibits a normal user to access user settings (change password, etc.).
+# Only sysadmins will be able to do so.
+# This also deactivates and hides the "Manage" button inside the user's profile page.
+def user_update(context, data_dict=None):
+    return {'success': False, 'msg': 'You are not authorized to update the user profile'}
+
 
 class Restrict_User_PermissionsPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IAuthFunctions)
 
     # IConfigurer
 
@@ -11,3 +18,6 @@ class Restrict_User_PermissionsPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'restrict_user_permissions')
+
+    def get_auth_functions(self):
+        return {'user_update': user_update}
