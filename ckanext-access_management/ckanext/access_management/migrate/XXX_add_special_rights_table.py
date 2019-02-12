90ba88defc375e2c43744bc2d78484052076252e
Odd Andersen <odd.andersen@sintef.no>
2019-02-12 10:17:18 +0100

    Fixed typo

--- ckanext-access_management/ckanext/access_management/migrate/XXX_add_special_rights_table.py
+++ ckanext-access_management/ckanext/access_management/migrate/XXX_add_special_rights_table.py
@@ -17,26 +17,26 @@ rights_table = Table(
     Column('user_id', UnicodeText, ForeignKey('user.id')),
     Column('package_id', UnicodeText, ForeignKey('package.id'))) 
 
-access_restriction_table = Table(
-    'access_restriction', meta,
-    Column('id', UnicodeText, primary_key=True, default=make_uuid),
-    Column('package_id', UnicodeText, ForeignKey('package.id'),
-           unique=True, nullable=False),
-    Column('restricted', Boolean, default=False),
-    Column('embargo_date', DateTime, default=None))
+# access_restriction_table = Table(
+#     'access_restriction', meta,
+#     Column('id', UnicodeText, primary_key=True, default=make_uuid),
+#     Column('package_id', UnicodeText, ForeignKey('package.id'),
+#            unique=True, nullable=False),
+#     Column('restricted', Boolean, default=False),
+#     Column('embargo_date', DateTime, default=None))
 
 def upgrade(migrate_engine):
 
     meta.bind = migrate_engine
     with warnings.catch_warnings():
-        warnings.simplefilter("ignore", category=wqlalchemy.exc.SAWarning)
+        warnings.simplefilter("ignore", category=sqlalchemy.exc.SAWarning)
         user_table = Table('user', meta, autoload=True)
         package_table = Table('package', meta, autoload=True)
 
     rights_table.create()
-    access_restriction_table.create()
+    # access_restriction_table.create()
 
 def downgrade(migrate_engine):
     meta.bind = migrate_engine
     rights_table.drop()
-    access_restriction_table.drop()
+    # access_restriction_table.drop()
