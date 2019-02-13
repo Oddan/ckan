2134a5a0d69ca9ea763ff1b9f193fc6451e5df33
FilippoRemonato <filippo.remonato@sintef.no>
2019-02-13 11:19:14 +0100

    PLUGIN: DATASETS_LANDING_PAGE. Cleaned up the code a little

--- ckanext-datasets_landing_page/ckanext/datasets_landing_page/plugin.py
+++ ckanext-datasets_landing_page/ckanext/datasets_landing_page/plugin.py
@@ -7,11 +7,8 @@ import requests
 from ckan.common import c
 import ckan.model as model
 
-import pdb
 
-# This function sets the authorisation to download a dataset
-def user_download_dataset(context, data_dict=None):
-    return {'success': False, 'msg': 'You are not authorized to download the dataset.'}
+
 
 def download_multiple_resources():
     access = True
@@ -20,42 +17,31 @@ def download_multiple_resources():
         memory_file = io.BytesIO()
         with zipfile.ZipFile(memory_file, mode='w', compression=zipfile.ZIP_STORED) as zf:
             for res_name in request.form:
-                #pdb.set_trace()
-                #res = res.encode('utf-8')
+
                 f = requests.get(request.form[res_name], allow_redirects=True, headers={'Authorization': c.userobj.apikey}) # OBS: The function c is from Pylons. How to substitute with h?
-                print(' ')
-                print repr(res_name)
-                print(f.status_code)
-                print(' ')
-                #pdb.set_trace()
                 if f.status_code == 404:
                     return render_template(u"package/download_denied.html")
                 zf.writestr(res_name, f.content)
+
         memory_file.seek(0) # return to beginning of file <----------- is this needed??
         
     return send_file(memory_file, mimetype='application/zip', as_attachment = True, attachment_filename= 'download.zip')
 
 
 
+
+
 class Datasets_Landing_PagePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.IAuthFunctions)
     plugins.implements(plugins.IBlueprint)
+
 
     # IConfigurer
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        #toolkit.add_public_directory(config_, '../../../ckan/public/test_dataset_public_files')
         toolkit.add_resource('fanstatic', 'datasets_landing_page')
-
-
-    # IAuthFunctions
-
-    def get_auth_functions(self):
-        return {'user_download_dataset': user_download_dataset}
-
 
 
     # IBlueprint
