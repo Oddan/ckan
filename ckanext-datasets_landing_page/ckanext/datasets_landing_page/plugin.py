701ad59450f2373a4876285ddfdad8996e90cbbe
FilippoRemonato <filippo.remonato@sintef.no>
2019-02-15 16:25:00 +0100

    PLUGIN: DATASETS_LANDING_PAGE. Implemented confirmation that the user has read the licence before downloading resources. Fixed the css style for resource diplay. Wrote comments on what the plugin does

--- ckanext-datasets_landing_page/ckanext/datasets_landing_page/plugin.py
+++ ckanext-datasets_landing_page/ckanext/datasets_landing_page/plugin.py
@@ -7,7 +7,20 @@ import requests
 from ckan.common import c
 import ckan.model as model
 
+import pdb
 
+'''
+This plugin does the following:
+    - Implements the Datasets Landing Page. This is the page the user will see when clicking ona a dataset
+    - The Dataset Langin Page loads the description of the dataset from a custom-made html. The file
+      is either provided by the customer or made by the consortium, and is located in a folder in CKAN's public directory. The path to the description file
+      must be supplied when the dataset is created in one of the "extra fields" using custom_html_desc
+      as key for the field.
+    - Implements a checkbox form for the user to select and download resources.
+      The download function zips the resources on-the-fly, in memory, and provides the file for download.
+      The file is deleted and the memory freed after the download finishes.
+      The user must accept the dataset's licence to be able to download the files.
+'''
 
 
 def download_multiple_resources():
@@ -17,13 +30,12 @@ def download_multiple_resources():
         memory_file = io.BytesIO()
         with zipfile.ZipFile(memory_file, mode='w', compression=zipfile.ZIP_STORED) as zf:
             for res_name in request.form:
-
                 f = requests.get(request.form[res_name], allow_redirects=True, headers={'Authorization': c.userobj.apikey}) # OBS: The function c is from Pylons. How to substitute with h?
                 if f.status_code == 404:
                     return render_template(u"package/download_denied.html")
                 zf.writestr(res_name, f.content)
 
-        memory_file.seek(0) # return to beginning of file <----------- is this needed??
+        memory_file.seek(0)
         
     return send_file(memory_file, mimetype='application/zip', as_attachment = True, attachment_filename= 'download.zip')
 
