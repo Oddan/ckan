387823c6ae02b9f213bc8c55cf9e5dca4517f298
Odd Andersen <odd.andersen@sintef.no>
2019-05-15 13:29:04 +0200

    Fixed functionality to let users delete own profile

--- ckanext-remove_unwanted_features/ckanext/remove_unwanted_features/plugin.py
+++ ckanext-remove_unwanted_features/ckanext/remove_unwanted_features/plugin.py
@@ -1,5 +1,13 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import ckan.lib.helpers as h
+import ckan.logic as logic
+import ckan.lib.base as base
+import ckan.model as model
+from ckan.common import g, _, session, request
+from flask import Blueprint
+
+import pdb
 
 # If this function is used, it prohibits a normal user to access user settings (change password, etc.).
 # Only sysadmins will be able to do so.
@@ -9,12 +17,44 @@ def user_update(context, data_dict=None):
 
 # If this function is used, it allows users to delete their own profiles
 def user_delete(context, data_dict=None):
-    return {'success': True, 'msg':''}
+    
+    if data_dict and g.userobj.id == data_dict.get('id', None):
+        return {'success': True, 'msg': 'Users allowed to delete their own profile.'}
+
+    return {'success': False, 'msg': 'Users only allowed to delete their own profile.'}
+
+def logout_and_delete():
+    # delete user here
+    context = {
+        u'model': model,
+        u'session': model.Session,
+        u'user': g.user,
+        u'auth_user_obj': g.userobj
+    }
+    data_dict = {u'id': g.userobj.id}
+
+    # delete user
+    try:
+        logic.get_action(u'user_delete')(context, data_dict)
+    except logic.NotAuthorized:
+        msg = _(u'Unauthorized to delete used with id "{user_id}".')
+        base.abort(403, msg.format(user_id = g.userobj.id));
+
+    # log out by removing stale cookie (we cannot use repoze.who, likely since user has been deleted)
+
+    response = h.redirect_to(u'user.index')
+    for cookie in request.cookies:
+        if cookie == u'auth_tkt':
+            response.delete_cookie(cookie)
+            break
+
+    return response
 
 
 class Remove_Unwanted_FeaturesPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IAuthFunctions)
+    plugins.implements(plugins.IBlueprint)
 
     # IConfigurer
 
@@ -23,8 +63,20 @@ class Remove_Unwanted_FeaturesPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'remove_unwanted_features')
 
-
     # IAuthFunctions
 
     def get_auth_functions(self):
-        return {'user_delete': user_delete} #Dictionary containing which of the custom functions defined above to use
+        return {'user_delete': user_delete}
+
+    # IBlueprint
+
+    def get_blueprint(self):
+
+        blueprint = Blueprint(self.name, self.__module__)
+        
+        # only use POST method (not GET) method below to avoid accidental user
+        # deletion by entering the wrong link
+        blueprint.add_url_rule(u'/logout_and_delete', u'logout_and_delete',
+                               logout_and_delete, methods=['GET', 'POST'])
+        return blueprint
+    
