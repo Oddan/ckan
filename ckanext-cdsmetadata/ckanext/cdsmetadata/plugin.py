67efdefc060af04c4a6ba9c0a243c720df8c69da
Odd <odd.andersen@sintef.no>
2019-07-25 15:47:30 +0200

    Added functionality for viewing individual Persons

--- ckanext-cdsmetadata/ckanext/cdsmetadata/plugin.py
+++ ckanext-cdsmetadata/ckanext/cdsmetadata/plugin.py
@@ -25,7 +25,6 @@ data_format_table = None
 license_table = None
 publication_table = None
 person_table = None
-# user_extra_table = None # @@ remove
 organization_extra_table = None
 
 
@@ -34,6 +33,7 @@ contact_org_association_table = None  # associate contact Person with Organizati
 contact_dataset_association_table = None # associate contact with Dataset
 person_contributor_dataset_association_table = None # contributor (Person) with dataset
 org_contributor_dataset_association_table = None # contributor (Organization) with dataset
+
 
 class Person(model.domain_object.DomainObject):
     def __init__(self, first_name, last_name, email):
@@ -50,14 +50,6 @@ class OrganizationExtra(model.domain_object.DomainObject):
     def __init__(self, homepageURL, org_id):
         self.homepageURL = homepageURL
         self.org_id = org_id
-    
-
-# # @@ remove
-# class UserExtra(model.domain_object.DomainObject):
-#     def __init__(self, first_name, last_name, user_id):
-#         self.first_name = first_name
-#         self.last_name = last_name
-#         self.user_id = user_id
 
 
 class DataFormat(model.domain_object.DomainObject):
@@ -85,7 +77,6 @@ def setup_model():
 
     # object tables
     prepare_data_format_table()
-    # prepare_user_extra_table() # @@ remove
     prepare_organization_extra_table()
     prepare_license_table()
     prepare_publication_table()
@@ -112,6 +103,7 @@ def prepare_org_contributor_dataset_association_table():
 
         # create table
         ensure_table_created(org_contributor_dataset_association_table)
+
 
 def prepare_person_contributor_dataset_association_table():
 
@@ -302,29 +294,6 @@ def prepare_organization_extra_table():
         # create table
         ensure_table_created(organization_extra_table)
 
-# # @@ delete
-# def prepare_user_extra_table():
-
-#     global user_extra_table
-#     if user_extra_table is None:
-#         user_extra_table = Table(
-#             'user_extra', meta.metadata,
-#             Column('id', UnicodeText, primary_key=True, default=make_uuid),
-#             Column('first_name', UnicodeText, nullable=False),
-#             Column('last_name', UnicodeText, nullable=False),
-#             Column('user_id', UnicodeText, ForeignKey('user.id'))
-#         )
-
-#         meta.mapper(UserExtra, user_extra_table,
-#                     properties={'user':
-#                                 orm.relation(model.user.User,
-#                                              backref=orm.backref(
-#                                                  'extra',
-#                                                  uselist=False,
-#                                                  cascade='all, delete, delete-orphan'))})
-
-#     ensure_table_created(user_extra_table)
-
 
 def ensure_table_created(table):
 
@@ -382,58 +351,6 @@ def _organization_modif_wrapper(action_name):
     return _wrapper
 
 
-# def _user_modif_wrapper(action_name):
-
-#     action = tk.get_action(action_name)
-
-#     def _wrapper(context, data_dict):
-
-#         first_name = data_dict.get('first_name', '')
-#         last_name = data_dict.get('last_name', '')
-
-#         # sync 'fullname' with 'first_name' and last_name'
-#         if 'fullname' not in data_dict.keys():
-#             data_dict['fullname'] = '{0} {1}'.format(first_name, last_name)
-
-#         result_dict = action(context, data_dict)
-
-#         result_dict['first_name'] = first_name
-#         result_dict['last_name'] = last_name
-
-#         # updating the extra information (not found in the original user
-#         # object)
-
-#         new_extra = model.User.get(result_dict['id']).extra
-#         if new_extra is None:
-#             new_extra = UserExtra(first_name, last_name, result_dict['id'])
-#         else:
-#             new_extra.first_name = first_name
-#             new_extra.last_name = last_name
-#         new_extra.save()
-
-#         return result_dict
-
-#     return _wrapper
-
-
-# # @@ must change
-# def _user_show_wrapper():
-
-#     action = tk.get_action('user_show')
-
-#     def _wrapper(context, data_dict):
-
-#         result_dict = action(context, data_dict)
-#         extra = model.User.get(result_dict['id']).extra
-#         if extra is not None:
-#             result_dict['first_name'] = extra.first_name
-#             result_dict['last_name'] = extra.last_name
-
-#         return result_dict
-
-#     return _wrapper
-
-
 def _organization_show_wrapper():
 
     action = tk.get_action('organization_show')
@@ -457,9 +374,14 @@ def _organization_show_wrapper():
             [(x.id, x.name, x.email) for x in org.people]
 
         if new_extra is not None:
-            result_dict['datasets_contributed_to'] = \
-                [(x.id, x.title) for x in new_extra.datasets_contributed_to]
 
+            result_dict['datasets_contributed_to'] = \
+                [[x.id, x.title, x.owner_org]
+                 for x in new_extra.datasets_contributed_to]
+            for li in result_dict['datasets_contributed_to']:
+                owner_org = model.Group.get(li[2])
+                li[2] = "" if owner_org is None else owner_org.title
+            
         if result_dict['contact_person'] is not None:
             result_dict['contact_person'].sort(key=lambda x: x[1])
         result_dict['contact_person_listitems'] = \
@@ -471,75 +393,12 @@ def _organization_show_wrapper():
             _personlist([x[0] for x in result_dict.get('people', [])])
 
         result_dict['dataset_contributions_listitems'] = \
-            _dsetlist([x[0] for x in result_dict.get('datasets_contributed_to', [])])
+            _dsetlist(
+                [x[0] for x in result_dict.get('datasets_contributed_to', [])])
 
         return result_dict
 
     return _wrapper
-
-
-# @@ this function at present only used when listing members of an org.
-# Must be adapted to refer to a Person object rather than a CKAN User
-# def _username_helper_fun(user, maxlength=0):
-#     if not isinstance(user, model.User):
-#         user_name = text_type(user)
-#         result = user_name
-#         user = model.User.get(user_name)
-#         if user:
-#             result = user.name
-#     else:
-#         result = user.name
-
-#     if maxlength and len(result) > maxlength:
-#         result = result[:maxlength] + '...'
-
-#     return result
-
-
-# # @@ Used in organization form to facilitate designation of contact person.
-# # Will have to change.
-# def autocomplete_filtered():
-
-#     q = request.args.get(u'q', u'')
-#     org_id = request.args.get(u'org_id', u'')
-#     limit = request.args.get(u'limit', 20)
-#     user_list = []
-#     if q:
-#         context = {u'model': model, u'session': model.Session,
-#                    u'user': g.user, u'auth_user_obj': g.userobj}
-
-#         data_dict = {u'q': q, u'limit': limit}
-
-#         # get list with matching user names
-#         user_list = tk.get_action(u'user_autocomplete')(context,
-#                                                         data_dict)
-
-#         # narrow down to list of users who are actual members
-#         if org_id:
-#             member_list = tk.get_action('member_list')(
-#                 {'model': model}, {'id': org_id})
-#             member_ids = [x[0] for x in member_list]
-#             user_list = [u for u in user_list if u['id'] in member_ids]
-
-#     return api._finish_ok(user_list)
-
-
-# # @@ This must change to refer to the new Person table, not to users
-# def contact_person_validator(value, context):
-
-#     org_id = context['group'].id
-#     member_list = tk.get_action('member_list')(
-#         {'model': model}, {'id': org_id})
-#     member_ids = [x[0] for x in member_list]
-
-#     user = model.User.by_name(value)
-#     if user is None:
-#         raise Invalid(_('Unknown user entered as contact person.'))
-
-#     if user.id not in member_ids:
-#         raise Invalid(_('Specified user is not member of the organization.'))
-
-#     return value
 
 
 def _edit_metadata_auth(context, data_dict=None):
@@ -565,7 +424,6 @@ def check_edit_metadata():
 
 def _ensure_list(obj):
     return obj if isinstance(obj, list) else [obj]
-
 
 
 def _list_people(session, people_ids):
@@ -793,7 +651,7 @@ def _data_format_delete(context, data_dict):
 
 
 def _person_show(context, data_dict):
-    tk.check_access('edit_metadata', context, data_dict)
+    # tk.check_access('edit_metadata', context, data_dict)
     per = context['session'].query(Person).get(data_dict['id'])
     if per is None:
         raise tk.ObjectNotFound
@@ -809,7 +667,7 @@ def _person_show(context, data_dict):
 
 
 def _publication_show(context, data_dict):
-    tk.check_access('edit_metadata', context, data_dict)
+    # tk.check_access('edit_metadata', context, data_dict)
     pub = context['session'].query(Publication).get(data_dict['id'])
     if pub is None:
         raise tk.ObjectNotFound
@@ -820,7 +678,7 @@ def _publication_show(context, data_dict):
 
 
 def _license_show(context, data_dict):
-    tk.check_access('edit_metadata', context, data_dict)
+    # tk.check_access('edit_metadata', context, data_dict)
     lic = context['session'].query(License).get(data_dict['id'])
     if lic is None:
         raise tk.ObjectNotFound
@@ -831,7 +689,7 @@ def _license_show(context, data_dict):
 
 
 def _data_format_show(context, data_dict):
-    tk.check_access('edit_metadata', context, data_dict)
+    # tk.check_access('edit_metadata', context, data_dict)
 
     df = context['session'].query(DataFormat).get(data_dict['id'])
     if df is None:
@@ -914,7 +772,8 @@ def _dsetlist(selected_ids):
 
     datasets = model.Session.query(model.package.Package).all()
     dsetlist = \
-        [{'value': x.id, 'text': x.title, 'selected': False} for x in datasets]
+        [{'value': x.id, 'text': x.title, 'selected': False}
+         for x in datasets]
     dsetlist.sort(key=lambda x: x['text'])
 
     # set selection
@@ -968,6 +827,11 @@ def _extract_metadata_form_data(form, mclass):
 
     return data_dict
 
+
+def _display_person(id):
+    data = _person_show({'session': Session}, {'id': id})
+    
+    return render('view_person.html', data)
 
 def _edit_metadata(mclass, template_name):
     check_edit_metadata()  # check authorization
@@ -1041,18 +905,11 @@ class CdsmetadataPlugin(plugins.SingletonPlugin,
     def get_auth_functions(self):
         return{'edit_metadata': _edit_metadata_auth}
 
-    # # ================================ IValidators ============================
-    # def get_validators(self):
-
-    #     return {'valid_contact_person': contact_person_validator}
-
     # ================================ IBlueprint =============================
 
     def get_blueprint(self):
         blueprint = Blueprint(self.name, self.__module__)
         blueprint.template_folder = u'templates'
-        # blueprint.add_url_rule('/api/co2datashare/autocomplete_filtered',
-        #                        view_func=autocomplete_filtered)
         blueprint.add_url_rule('/metadata/dataformat',
                                view_func=_edit_dataformat,
                                methods=['GET', 'POST'])
@@ -1065,27 +922,23 @@ class CdsmetadataPlugin(plugins.SingletonPlugin,
         blueprint.add_url_rule('/metadata/person',
                                view_func=_edit_person,
                                methods=['GET', 'POST'])
-        return blueprint
 
-    # # ============================= ITemplateHelpers ==========================
-    # def get_helpers(self):
-    #     # @@ currently only used when list
-    #     return {'username': _username_helper_fun}
+        blueprint.add_url_rule('/view/person/<id>',
+                               u'view_person_info',
+                               view_func=_display_person)
+
+
+        return blueprint
 
     # ================================= IActions ==============================
     def get_actions(self):
 
         result = {}
-        # for aname in ['user_create', 'user_update']:
-        #     result[aname] = _user_modif_wrapper(aname)
-
-        # result['user_show'] = _user_show_wrapper()
 
         for aname in ['organization_create', 'organization_update']:
             result[aname] = _organization_modif_wrapper(aname)
         result['organization_show'] = _organization_show_wrapper()
 
-        
         result['dataformat_create'] = _data_format_create
         result['dataformat_update'] = _data_format_update
         result['dataformat_show'] = _data_format_show
@@ -1121,112 +974,3 @@ class CdsmetadataPlugin(plugins.SingletonPlugin,
         tk.add_resource('fanstatic', 'cdsmetadata')
         # tk.add_public_directory(config_, 'public')
 
-    # # ================================ IGroupForm =============================
-    # is_organization = True
-
-    # def is_fallback(self):
-
-    #     return True
-
-    # def group_types(self):
-
-    #     return []
-
-    # def group_controller(self):
-
-    #     pass
-
-    # def form_to_db_schema(self):
-
-    #     schema = super(CdsmetadataPlugin, self).form_to_db_schema()
-    #     schema.update({'homepageURL': [
-    #                         tk.get_validator('ignore_missing'),
-    #                         tk.get_converter('convert_to_extras')
-    #                                   ]})
-    #     return schema
-
-    # def db_to_form_schema(self):
-    #     schema = self._default_show_group_schema()
-
-    #     schema.update({'homepageURL': [
-    #         tk.get_converter('convert_from_extras'),
-    #         tk.get_validator('ignore_missing')
-    #     ]})
-    #     return schema
-
-    # def _default_show_group_schema(self):
-    #     schema = _schema.default_group_schema()
-
-    #     # make default show schema behave like when run with no validation
-    #     schema['num_followers'] = []
-    #     schema['created'] = []
-    #     schema['display_name'] = []
-    #     # schema['extras'] = {'__extras': [keep_extras]}  # has to be removed, or extras won't work
-    #     schema['package_count'] = [tk.get_validator('ignore_missing')]
-    #     schema['packages'] = {'__extras': [tk.get_validator('keep_extras')]}
-    #     schema['revision_id'] = []
-    #     schema['state'] = []
-    #     schema['users'] = {'__extras': [tk.get_validator('keep_extras')]}
-
-    #     return schema
-
-
-
-
-# def _orglist(person_data):
-
-#     orgs = model.Session.query(model.group.Group).all()
-#     orglist = \
-#         [{'value': x.id, 'text': x.title, 'selected': False} for x in orgs]
-#     orglist.sort(key=lambda x: x['text'])
-
-#     # prepare list where all current affiliations are selected
-#     afflist = \
-#         [] if person_data is None else [x[0] for x in person_data['affiliation']]
-#     orglist_aff = copy.deepcopy(orglist) 
-
-#     for o in orglist_aff:
-#         if o['value'] in afflist:
-#             o['selected'] = True
-
-#     # prepare list where all current contact organizations are listed
-#     contact_list = \
-#         [] if person_data is None else [x[0] for x in person_data['contact_org']]
-#     orglist_contact = copy.deepcopy(orglist)
-#     for o in orglist_contact:
-#         if o['value'] in contact_list:
-#             o['selected'] = True
-
-#     return {'orglist_aff': orglist_aff, 'orglist_contact': orglist_contact}
-
-
-# def _dsetlist(person_data):
-
-#     datasets = model.Session.query(model.package.Package).all()
-#     dsetlist = \
-#         [{'value': x.id, 'text': x.title, 'selected': False} for x in datasets]
-#     dsetlist.sort(key=lambda x: x['text'])
-
-#     # prepare list where all current contact datasets are listed
-#     contact_list = \
-#         [] if person_data is None \
-#         else [x[0] for x in person_data['contact_dataset']]
-
-#     dsetlist_contact = copy.deepcopy(dsetlist)
-#     for d in dsetlist_contact:
-#         if d['value'] in contact_list:
-#             d['selected'] = True
-
-#     # prepare list where all current contributor datasets are listed
-#     contributor_list = \
-#         [] if person_data is None \
-#         else [x[0] for x in person_data['contributor_dataset']]
-
-#     dsetlist_contributor = copy.deepcopy(dsetlist)
-#     for d in dsetlist_contributor:
-#         if d['value'] in contributor_list:
-#             d['selected'] = True
-
-#     return {'dsetlist_contact': dsetlist_contact,
-#             'dsetlist_contributor': dsetlist_contributor}
-    
