b8bc960d22315362778c2b03f6fb320abf9153e0
Odd Andersen <odd.andersen@sintef.no>
2019-08-09 11:53:37 +0200

    Slight renaming of docker image and development setup script

--- contrib/docker/scripts/setup_developer_mode.sh
+++ contrib/docker/scripts/setup_developer_mode.sh
@@ -18,7 +18,7 @@ ckan-paster make-config --no-interactive ckan "$CONFIG"
 
 # modifying configuration file
 sed 's/^debug =.*$/debug = true/' /etc/ckan/production.ini | \
-sed 's/^ckan.site_url =.*$/ckan.site_url = http:\/\/localhost:5000/'  > /etc/ckan/production.ini.2
+sed 's/^ckan.site_url =.*$/ckan.site_url = http:\/\/localhost:5000/'  > /etc/ckan/development.ini
 
 # installing ckanext_spatial stuff
 ckan-pip install -e "git+https://github.com/ckan/ckanext-spatial.git#egg=ckanext-spatial"
@@ -26,15 +26,15 @@ ckan-pip install -r $CKAN_VENV/src/ckanext-spatial/pip-requirements.txt
 echo "In your configuration file, remember to add 'spatial_metadata' and 'spatial_query' to your plugin list, and introduce 'ckanext.spatial.search_backend = solr-spatial-field'"
 
 # setting up database
-ckan-paster --plugin=ckan db init -c "${CKAN_CONFIG}/production.ini"
+ckan-paster --plugin=ckan db init -c "${CKAN_CONFIG}/development.ini"
 
 # setup sysadmin
 sleep 8 # wait for database to complete
 cd $CKAN_VENV/src/ckan
-ckan-paster --plugin=ckan user add admin email=admin@localhost name=admin password=passpass -c /etc/ckan/production.ini
-ckan-paster --plugin=ckan sysadmin add admin -c /etc/ckan/production.ini
+ckan-paster --plugin=ckan user add admin email=admin@localhost name=admin password=passpass -c /etc/ckan/development.ini
+ckan-paster --plugin=ckan sysadmin add admin -c /etc/ckan/development.ini
 cd -
 
 # restarting server
-# ckan-paster serve --reload /etc/ckan/production.ini
-ckan-paster serve /etc/ckan/production.ini
+# ckan-paster serve --reload /etc/ckan/development.ini
+ckan-paster serve /etc/ckan/development.ini
