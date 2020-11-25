2fc3e8432a551cf0792a6b154038d15f2b7b6c9a
Odd <odd.andersen@sintef.no>
2020-11-26 00:59:08 +0100

    Work on sigma2 metadata preparation milestone reached
    Access to the Sigma2 API will now be needed to complete the task

--- ckanext-cdsmetadata/ckanext/cdsmetadata/cdssigma2_plugin.py
+++ ckanext-cdsmetadata/ckanext/cdsmetadata/cdssigma2_plugin.py
@@ -7,10 +7,12 @@ from ckan.lib.base import abort
 from ckan.logic.converters import convert_package_name_or_id_to_id
 from ckan.common import g
 import json
+from copy import deepcopy
 from flask import Blueprint
 from os import path
 from cdsaccess_plugin import SpecialAccessRights
-from plugin import License, Publication
+from resource_category import ResourceCategory
+from plugin import License, Publication, DataFormat, Person
 from landing_page_plugin import landing_page_location
 
 import pdb
@@ -28,6 +30,7 @@ def extract_sigma2_metadata(pkg_info):
 
     return {'datasets': datasets,
             'persons': sigma2_person_metadata(person_ids),
+            'publications' : sigma2_publication_metadata(pub_ids),
             'organizations': sigma2_organization_metadata(org_ids),
             'licenses': sigma2_license_metadata(license_ids)}
 
@@ -50,12 +53,17 @@ def _remove_duplicate(lst):
 
 def sigma2_parent_dataset_metadata(pkg_info):
 
+    context = {'model': model, 'session': model.Session,
+               'user': g.user, 'for_view': True,
+               'auth_user_obj': g.userobj}
+    
     person_ids, org_ids, license_ids, pub_ids = [], [], [], []
 
     # constant metadata (always the same for export to Sigma2)
     language = 'English'  # always English
     category = 'Observation'  # we use 'Observation' as default.
-    journal = ''  # we use BibliographicCitation for citations
+    journal = ''
+    bibliographic_citation = '' # how should this dataset be cited
     subject = 'geological CO2 storage'  # always, for now
     project = 'CO2DataShare'
 
@@ -122,7 +130,7 @@ def sigma2_parent_dataset_metadata(pkg_info):
         description += 'Related dataset(s):\n'
         for rd in pkg_info['related_dataset']:
             rel_pkg = tk.get_action('package_show')\
-                (context, {'id': rd})
+                (context, {'id': rd[0]})
             if rel_pkg:
                 description += rel_pkg['title'] + '\n'
         description += '\n'
@@ -150,52 +158,190 @@ def sigma2_parent_dataset_metadata(pkg_info):
                  'Identifier': identifier,
                  'Title': title}
 
-    optional = {'BibliographicCitation': citations,
+    optional = {'BibliographicCitation': bibliographic_citation,
                 'Geolocation': location,
                 'Project': project,
-                'Temporal Coverage': temporal_coverage}
+                'Temporal Coverage': temporal_coverage,
+                'Publication': citations}  # citations not strictly a part of 'optional'
+    hierarchy = {'id': pkg_info['id'],
+                 'HasPart': [x['id'] for x in pkg_info['resources']]}
 
     return \
-        {'mandatory': mandatory, 'optional': optional}, \
+        {'mandatory': mandatory,
+         'optional': optional,
+         'hierarchy': hierarchy}, \
         _remove_duplicate(person_ids), \
         _remove_duplicate(org_ids), \
         _remove_duplicate(license_ids), \
         _remove_duplicate(pub_ids)
 
 
+def _dataformat_text(df_id):
+    df = model.Session.query(DataFormat).get(df_id)
+
+    if df is None:
+        return '<data format not found>'
+    else:
+        result = 'Data format name: ' + df.name + '\n'
+        if df.is_open:
+            result += 'This is an open format.\n'
+        else:
+            result += 'This is not an open format.\n'
+
+        result += 'Data format description:\n' + df.description + '\n'
+
+        return result
+
+
+def _category_from_class(classcode):
+
+    # The Sigma2 Category will be set as follows:
+    # 1.X.X -> "Observation"
+    # 2.X.X -> "Experiment"
+    # 3.1.0 -> "Model"
+    # 3.2.0 -> "Model"
+    # 3.3.X -> "Simulation"
+    # 4.X.X -> "Observation"
+    # (Valid Sigma2 categories are: Experiment, Observation, Model, Simulation,
+    # Software, Image, Calibration)
+    classcode = classcode.split('.')
+    if classcode[0] == '1':
+        return 'Observation'
+    elif classcode[0] == '2':
+        return 'Experiment'
+    elif classcode[0] == '3':
+        if classcode[1] == '1' or classcode[1] == '2':
+            return 'Model'
+        else:
+            return 'Simulation'
+    else:
+        return 'Observation'
+
+
 def sigma2_dataset_component_metadata(pkg_info, parent_dataset):
+    result = []
 
-    # components =  # extract info for components here
-    # own_ID = #
-    # owner_ID = #
-    # part_IDs = # extract IDs from components
+    components = pkg_info['resources']
 
-    # component_items = {'co2datashare_id' : own_ID,
-    #                    'hasPart': part_IDs,
-    #                    'isPartOf': owner_ID,
-    #                    'components': components}
+    for comp in components:
 
-    pass
+        child_dataset = deepcopy(parent_dataset)
+        child_dataset['hierarchy'] = {
+            'id': comp['id'],
+            'IsPartOf': parent_dataset['hierarchy']['id']
+        }
 
+        # update title with resource name
+        title = child_dataset['mandatory']['Title'] + ' - ' + comp['name']
+
+        child_dataset['mandatory']['Title'] = title
+
+        # assemble and update description
+        description = comp['description']
+        if len(description) > 0:
+            description += '\n\n'
+        if len(comp['purpose']) > 0:
+            description += 'Purpose:\n' + comp['purpose'] + '\n\n'
+        if len(comp['assumptions']) > 0:
+            description += 'Assumptions:\n' + comp['assumptions'] + '\n\n'
+        if len(comp['dataformat']) > 0:
+            description += \
+                'Data format:\n' + _dataformat_text(comp['dataformat']) + '\n\n'
+        category = model.Session.query(ResourceCategory).get(comp['category'])
+        if category:
+            description += \
+                'CO2DataShare category is: \n   ' + \
+                category.code + ' : ' + category.title + '\n'
+
+
+        child_dataset['mandatory']['Description'] = description
+
+        # set more precise category based on data classification
+        child_dataset['mandatory']['Category'] = \
+            _category_from_class(comp['category'])
+
+        result.append(child_dataset)
+
+    return result
 
 
 def sigma2_person_metadata(person_ids):
-    pass
 
+    result = []
+    for p_id in person_ids:
+        person = model.Session.query(Person).get(p_id)
+        if person:
+            affiliation = ''
+            if len(person.affiliation) > 0:
+                # @@ NB: in case of multiple affiliations, only the first one
+                # is returned, since only one is supported by Sigma2
+                affiliation = person.affiliation[0].display_name
 
-def sigma2_organization_metadata(org_ids):
-    pass
+            result.append({'id': p_id,
+                           'FirstName': person.first_name,
+                           'LastName': person.last_name,
+                           'Email': person.email,
+                           'OrgShortName': affiliation})
+    return result
 
 
 def sigma2_license_metadata(license_ids):
-    pass
+
+    result = []
+    for l_id in license_ids:
+        license = model.Session.query(License).get(l_id)
+        if license:
+            result.append({'id': l_id,
+                           'Access': license.license_url,
+                           'Archive': license.license_url,
+                           'Name': license.name,
+                           'Description': license.description})
+    return result
 
 
+def sigma2_organization_metadata(org_ids):
+
+    result = []
+    for o_id in org_ids:
+        org = model.Session.query(model.group.Group).get(o_id)
+        if org:
+
+            contact_first_name, contact_last_name, contact_email = '', '', ''
+            if len(org.contact_person) > 0:
+                # if there are multiple contact persons, we can only return the
+                # first
+                contact_first_name = org.contact_person[0].first_name
+                contact_last_name = org.contact_person[0].last_name
+                contact_email = org.contact_person[0].email
+
+            result.append({'id': o_id,
+                           'OrgLongName': org.display_name,
+                           'OrgShortName': org.display_name,
+                           'HomePage': org.extra.homepageURL,
+                           'ContactFirstName': contact_first_name,
+                           'ContactLastName': contact_last_name,
+                           'ContactEmail': contact_email})
+    return result
+
+
+def sigma2_publication_metadata(pub_ids):
+
+    result = []
+    for p_id in pub_ids:
+        pub = model.Session.query(Publication).get(p_id)
+        if pub:
+            result.append({'id': p_id,
+                           'ConferenceCitation': pub.citation,
+                           'ConferenceURI': pub.doi,
+                           'JournalCitation': pub.citation,
+                           'JournalDOI': pub.doi,
+                           'Name': pub.name})
+    pdb.set_trace()
+    return result
 
 
 def export_package(pkg_name):
 
-    pdb.set_trace()
     # check credentials
     context = {'model': model, 'session': model.Session,
                'user': g.user, 'for_view': True,
